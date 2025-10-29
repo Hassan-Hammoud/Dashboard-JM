c9417b7ccea1aec55aed06628211b5045ffd7ccd
Hassan Hammoud <hassanhammoud2000@gmail.com>
2025-10-29 20:32:45 +0200

    Enhance Editor component with Rich Text Editor functionality

--- src/index.js
+++ src/index.js
@@ -6,7 +6,13 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { ContextProvider } from './contexts/ContextProvider';
 import './index.css';
-
+const resizeObserverErr =
+  /ResizeObserver loop completed|ResizeObserver loop limit exceeded/;
+const originalConsoleError = console.error;
+console.error = (...args) => {
+  if (resizeObserverErr.test(args[0])) return;
+  originalConsoleError.apply(console, args);
+};
 ReactDOM.render(
   <React.StrictMode>
     <ContextProvider>
