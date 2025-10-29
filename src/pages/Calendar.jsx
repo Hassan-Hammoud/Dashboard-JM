0a453a8d47ab22e45f13a9b95cef0a66357402e3
Hassan Hammoud <hassanhammoud2000@gmail.com>
2025-10-29 17:58:36 +0200

    Calendar component.

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,7 +1,35 @@
+import { Inject } from '@syncfusion/ej2-react-charts';
+import {
+  Agenda,
+  Day,
+  DragAndDrop,
+  Month,
+  Resize,
+  ScheduleComponent,
+  Week,
+  WorkWeek,
+} from '@syncfusion/ej2-react-schedule';
 import React from 'react';
-
+import { Header } from '../components';
+import { scheduleData } from '../data/dummy';
 const Calendar = () => {
-  return <div>Calendar</div>;
+  return (
+    <div className='m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl'>
+      <Header
+        category='App'
+        title='Calendar'
+      />
+      <ScheduleComponent
+        height='650px'
+        eventSettings={{ dataSource: scheduleData }}
+        selectedDate={new Date(2025, 0, 10)}
+      >
+        <Inject
+          services={[Day, Week, WorkWeek, Month, Agenda, Resize, DragAndDrop]}
+        />
+      </ScheduleComponent>
+    </div>
+  );
 };
 
 export default Calendar;
